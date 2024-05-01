187208294c228ca17b3d89e4faa50586ab4d849e
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-04-30 23:12:34 -0400

    fix

--- routers/scraper.py
+++ routers/scraper.py
@@ -6,16 +6,16 @@ from scraper.extract import ScrapeWebsite
 scraper_router = APIRouter(prefix="/scraper")
 
 
-@scraper_router.get("get-scrape-data")
-async def get_scrape_data(_: Request, scrape_query: ScrapeQuery = Body(...)):
+@scraper_router.get("/get-scrape-data")
+async def get_scrape_data(_: Request, source_id: str, link: str):
 
     try:
-        scraper = ScrapeWebsite(scrape_query.link)
+        scraper = ScrapeWebsite(link)
         scrape_data = scraper.return_article()
     except Exception as e:
         raise HTTPException(
-            status_code=400, detail="Could not scrape data due to timeout"
+            status_code=400, detail=f"Could not scrape data due to timeout: {e}"
         )
 
-    scrape_data = ScrapeData(source_id=scrape_query.source_id, content=scrape_data)
+    scrape_data = ScrapeData(source_id=source_id, content=scrape_data)
     return jsonable_encoder(scrape_data)
